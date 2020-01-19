4d63360cfbe51b850c55b4cf28a84419080099ef
王蕾蕾 <wangleilei01@zuoyebang.com>
2020-01-19 15:19:16 +0800

    Update deploy.sh
    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:JETWLL/one-week-one-book.git master:dev
+git push -f git@github.com:JETWLL/one-week-one-book.git master:gh-pages
 
 cd -
